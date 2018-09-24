3703f85d96cb5b82625996ebcf17ac2c6bb5c015
JeffreyRandom <35880312+JeffreyRandom@users.noreply.github.com>
2018-09-23 19:22:38 -0500

    Update conx.php

--- conx.php
+++ conx.php
@@ -1,10 +1,10 @@
-﻿<?php
+<?php
 // Cambia los datos por los tuyos.
-$servername = "sql308.epizy.com"; // Dejar en localhost, si sabes lo que haces, cámbialo.
-$username = "epiz_22749637"; // Nombre de usuario
-$pw = "maRZjCCQ"; // Contraseña
-$dbname = "epiz_22749637_gdpsdata"; // Nombre de la base de datos
+$servername = "localhost"; // Dejar en localhost, si sabes lo que haces, cámbialo.
+$username = ""; // Nombre de usuario de la base de datos
+$pw = ""; // Contraseña de la base de datos
+$dbname = ""; // Nombre de la base de datos
 
 // Seguridad
-$seguro = "itsj3ffre&" // Esta será una tipo contraseña, que se deberá poner la hacer acciones dentro de tu base de datos del servidor privado. Cuídala bien y compartela SÓLO con personas de TOTAL confianza.
+$seguro = "" // Esta será una tipo contraseña, que se deberá poner la hacer acciones dentro de tu base de datos del servidor privado. Cuídala bien y compartela SÓLO con personas de TOTAL confianza.
 ?>
