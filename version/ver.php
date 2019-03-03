09a0a72d50858648d6a94d97df64e1feca2037f8
JeffreyRandom <35880312+JeffreyRandom@users.noreply.github.com>
2019-03-03 10:45:57 -0500

    Update ver.php

--- version/ver.php
+++ version/ver.php
@@ -6,5 +6,5 @@
 
   */
 
-  $version = '<center><br><br><h3>AdminGDPS by <a href="https://youtube.com/JeffreyG" target="_blank">JeffreyG</a> (v0.8.1)</h3></center>'; // No interfiere con nada, sólo son créditos. Me haría feliz que no borraras esto. =')
+  $version = '<center><br><br><h3>AdminGDPS by <a href="https://youtube.com/JeffreyG" target="_blank">JeffreyG</a> (v0.8.2)</h3></center>'; // No interfiere con nada, sólo son créditos. Me haría feliz que no borraras esto. =')
 ?>
